92306cf0dd4ad3f640c5766c62e7e90134178d34
Ryan Kinney <ryan.kinney@maine.edu>
2021-02-25 14:55:11 -0500

    Write most of Part B User enters a list of numbers as command line arguments. The valid inputs are extracted and the LEDs are lit accordingly.

--- lab2/lab2b.c
+++ lab2/lab2b.c
@@ -18,10 +18,63 @@ void LED_setup(){
 	}
 }
 
+void set_leds(unsigned int num){
+	for(int i = 0; i < 8; ++i){
+		digitalWrite(i,(num&(1<<i))? HIGH:LOW);
+	}
+}
+
+
+// loop through the input arguments, flashing the LEDs accordingly
+// example: if the user enters 5 valid numbers, loop through them, setting the LEDS
+// to each one, with a small delay between them
 int main(int argc, char* argv[]){
 	// setup the required GPIO pins
 	wiringPiSetup();
 	LED_setup();
 
-	return 0;
+	// check for empty input
+	if(argc == 1){
+		printf("Error: Expected at least 1 argument, received none.\n");
+		printf("Usage is %s number1 number2 number3 ..., where numberX is an integer constant.\n",argv[0]);
+		set_leds(0);
+		exit(1);
+	}
+
+	// the input is not empty, loop through the input to save the valid arguments
+	// make an array to hold all the valid inputs
+	unsigned int valid[argc-1];
+	int size = 0; // the number of ints in valid
+	unsigned int number; // the extracted number from the arguments
+
+	for(int i = 1; i < argc; ++i){
+		int test;
+		// check for octal
+		if(test = sscanf(argv[i],"0%o",&number));
+		// not octal, check for hex
+		else if(test = sscanf(argv[i],"0x%x",&number));
+		// not hex, check for decimal
+		else if(test = sscanf(argv[i],"%u",&number));
+
+		// check validity of input
+		if(test==0 || number > 0xFF){
+			// invalid input, complain, do not increment counter, do not save it
+			printf("Error: Illegal input.\n");
+			printf("What was entered: %s\n",argv[i]);
+			printf("Expected an integer constant from 0-255 in decimal, 0-0xFF in hex, or 0-0377 in octal.\n");
+		}else
+			// save the input and increment the counter
+			valid[size++] = number;
+	}
+
+	// flash the LEDs
+	while(1){
+		// for each element in valid, set the LEDS to it, with a small delay
+		for(int i = 0; i < size; i++){
+			set_leds(valid[i]);
+			delay(50);
+		}
+	}
+
+	exit(0);
 }
