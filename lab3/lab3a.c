fcf45de7e0a031d5643bef11bf7ae6d873c893eb
Ryan Kinney <ryan.kinney@maine.edu>
2021-02-27 18:37:59 -0500

    Convert the loadavg to a "thermometer code" and light up LEDs

--- lab3/lab3a.c
+++ lab3/lab3a.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wiringPi.h>
+
+void led_setup(){
+	wiringPiSetup();
+
+	for(int i = 0; i < 8; ++i)
+		pinMode(i, OUTPUT);
+}
 
 int main(){
+
+	// program set up and error checking
+	led_setup();
 	FILE *fp;
 	fp = fopen("/proc/loadavg","r");
-
 	if(fp == NULL){
 		perror("Error");
 		exit(-1);
 	}
 
-	double loadAVG = 0.0;
+	// read the loadAVG and find the position of the most significant bit
+	double loadAVG;
 	fscanf(fp,"%lf",&loadAVG);
 
-	printf("%lf\n",loadAVG);
+	// since we want 4.0 in the loadavg to light up 8 LEDs
+	// multiply loadAVG by 64 so that 4.0 maps to more than 0xFF
+	int temp = ((int)(loadAVG*64.0))>>1;
+	int numLEDs = 0;
+
+	while(temp > 0){
+		temp >>= 1;
+		++numLEDs;
+	}
+
+	// numLEDs should be the number of LEDs to light up
+	int i;
+	for(i = 0; i < numLEDs && i < 8; ++i){ // light up LED 0-numLEDs
+		digitalWrite(i,HIGH);
+	}
+	for(i = numLEDs; i < 8; ++i){ // turn off LED numLEDs-7
+		digitalWrite(i,LOW);
+	}
 
 	fclose(fp);
 	exit(0);
