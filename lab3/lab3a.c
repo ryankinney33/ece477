251904c9976025b5b9a3d203c06111dc2f6eeff3
Ryan Kinney <ryan.kinney@maine.edu>
2021-02-27 18:08:48 -0500

    Start by reading the first number from /proc/loadavg

--- lab3/lab3a.c
+++ lab3/lab3a.c
@@ -3,14 +3,19 @@
 #include <unistd.h>
 
 int main(){
+	FILE *fp;
+	fp = fopen("/proc/loadavg","r");
 
+	if(fp == NULL){
+		perror("Error");
+		exit(-1);
+	}
 
+	double loadAVG = 0.0;
+	fscanf(fp,"%lf",&loadAVG);
 
+	printf("%lf\n",loadAVG);
 
-
-
-
-
-
+	fclose(fp);
 	exit(0);
 }
