3e42d4ebe15e7c8733fe059d03aa3337ea91522c
Ryan Kinney <ryan.kinney@maine.edu>
2021-02-27 19:07:49 -0500

    Display the loadavg as a thermometer code Reads the first number in /proc/loadavg. Converts determines the number of LEDs to light up based on it. Sets the LEDs accordingly. This repeats every 3 seconds until the program is terminated.

--- lab3/lab3a.c
+++ lab3/lab3a.c
@@ -11,39 +11,44 @@ void led_setup(){
 }
 
 int main(){
-
-	// program set up and error checking
+	// program set up
 	led_setup();
 	FILE *fp;
-	fp = fopen("/proc/loadavg","r");
-	if(fp == NULL){
-		perror("Error");
-		exit(-1);
+
+	while(1){
+		// open the loadavg file and check for error
+		fp = fopen("/proc/loadavg","r");
+		if(fp == NULL){
+			perror("Error");
+			exit(-1);
+		}
+		// read the loadAVG and find the position of the most significant bit
+		double loadAVG;
+		fscanf(fp,"%lf",&loadAVG);
+
+		// since we want 4.0 in the loadavg to light up 8 LEDs
+		// multiply loadAVG by 64 so that 4.0 maps to more than 0xFF
+		int temp = ((int)(loadAVG*64.0))>>1;
+		int numLEDs = 0;
+
+		while(temp > 0){
+			temp >>= 1;
+			++numLEDs;
+		}
+
+		// numLEDs should be the number of LEDs to light up
+		int i;
+		for(i = 0; i < numLEDs && i < 8; ++i){ // light up LED 0-numLEDs
+			digitalWrite(i,HIGH);
+		}
+		for(i = numLEDs; i < 8; ++i){ // turn off LED numLEDs-7
+			digitalWrite(i,LOW);
+		}
+
+		// close the file (for reopening)
+		fclose(fp);
+		sleep(3);
 	}
 
-	// read the loadAVG and find the position of the most significant bit
-	double loadAVG;
-	fscanf(fp,"%lf",&loadAVG);
-
-	// since we want 4.0 in the loadavg to light up 8 LEDs
-	// multiply loadAVG by 64 so that 4.0 maps to more than 0xFF
-	int temp = ((int)(loadAVG*64.0))>>1;
-	int numLEDs = 0;
-
-	while(temp > 0){
-		temp >>= 1;
-		++numLEDs;
-	}
-
-	// numLEDs should be the number of LEDs to light up
-	int i;
-	for(i = 0; i < numLEDs && i < 8; ++i){ // light up LED 0-numLEDs
-		digitalWrite(i,HIGH);
-	}
-	for(i = numLEDs; i < 8; ++i){ // turn off LED numLEDs-7
-		digitalWrite(i,LOW);
-	}
-
-	fclose(fp);
 	exit(0);
 }
