033918e09d787d1a7d7f212e290f7fd7d1f0c9d4
Ryan Kinney <ryan.kinney@maine.edu>
2021-03-11 08:09:28 -0500

    Write a starter LED cycling program

--- lab4/lab4a.c
+++ lab4/lab4a.c
@@ -4,6 +4,7 @@
 
 
 void gpio_init();
+void set_leds(int num);
 
 int main(){
 	gpio_init();
@@ -11,10 +12,12 @@ int main(){
 
 
 
-
-
-
-
+	while(1){
+		for(int i = 1; i < 129; i <<= 1){
+			set_leds(i);
+			delay(32);
+		}
+	}
 
 	exit(0);
 }
@@ -33,4 +36,14 @@ void gpio_init(){
 	// use internal pull down resistors
 	pullUpDnControl(21,PUD_DOWN);
 	pullUpDnControl(22,PUD_DOWN);
+}
+
+void set_leds(int num){
+	for(int i = 0; i < num; ++i){
+		if(num&(1<<i)){
+			digitalWrite(i,HIGH);
+		}else{
+			digitalWrite(i,LOW);
+		}
+	}
 }
