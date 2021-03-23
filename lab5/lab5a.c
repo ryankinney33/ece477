e3130b0067bdbe9c64fe21c30be7e94844ebdac7
Ryan Kinney <ryan.kinney@maine.edu>
2021-03-23 14:42:30 -0400

    Write a program to find the average frequency of a square wave

--- lab5/lab5a.c
+++ lab5/lab5a.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
+#include <time.h>
+
+#define SAMPLE_C 200
 
 // This function sets pin 0 as pull-down input
 void gpio_init();
@@ -8,26 +11,36 @@ void gpio_init();
 int main(){
 	gpio_init();
 
-	// a counter for how many loop iterations ran
-	long long unsigned int counter = 0;
+	// used for determining the time elapsed between two rising edges of the signal
+	clock_t timer[2]; // timer[0] is the start of the period, timer[1] is the end
+	double time[SAMPLE_C]; // the frequency is 1/time
 
-	int riseEdge = 0; // a flag for whether or not a rising edge was found
-	int currentState = 1, previousState = 1; // current and previous input states
-	// note, these are both initialized to 1 in order to ignore the initial state of the input pin
-	// when the program is started (if the input starts high, that will not count as a rising edge
+	for(int j = 0; j < SAMPLE_C; ++j){
+		// get the two times
+		for(int i = 0; i < 2; ++i){
+			int riseEdge = 0; // a flag for whether or not a rising edge was found
+			int currentState = 1, previousState = 1; // current and previous input states
+			// note, these are both initialized to 1 in order to ignore the initial state of the input pin
+			// when the program is started (if the input starts high, that will not count as a rising edge
 
-	while(!riseEdge){
-		// update the current and previous states
-		previousState = currentState;
-		currentState = digitalRead(0);
-		riseEdge = currentState && !previousState; // check for rising edge
-		counter++;
+			while(!riseEdge){
+				// update the current and previous states
+				previousState = currentState;
+				currentState = digitalRead(0);
+				riseEdge = currentState && !previousState; // check for rising edge
+			}
+			timer[i] = clock(); // set the time
+		}
+		// calculate the elapsed time and save it in the array
+		time[j] = ((double)(timer[1]-timer[0]))/CLOCKS_PER_SEC;
 	}
 
-	// rising edge found, print results
-	printf("Rising Edge detected!\n");
-	printf("Counter = %llu\n",counter);
-
+	// find the average frequency of the wave
+	double sum = 0.0; // start by finding the sum of the times
+	for(int i = 0; i < SAMPLE_C; ++i)
+		sum += time[i];
+	// the average frequency is the number of samples/sum of the times
+	printf("Frequency: %lf\n", (double)SAMPLE_C/sum);
 	exit(0);
 }
 
