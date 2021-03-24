c26009e778e69a422a6f0e5547ebb7820c865d3a
Ryan Kinney <ryan.kinney@maine.edu>
2021-03-24 11:01:33 -0400

    Move main() to another file

--- project/ClassicController.c
+++ project/ClassicController.c
@@ -4,17 +4,3 @@
 
 #include "ClassicController.h"
 
-int main() {
-	// initialize the gpio pins and setup I2C
-	int fd = wiringPiI2CSetup(DEVICE_ID);
-
-	if(fd == -1){
-		printf("Failed to initialize I2C communication.\n");
-		return 1;
-	}
-
-	printf("I2C communication successfully initialized.\n");
-
-	printf("%x\n",wiringPiI2CRead(fd));
-	return 0;
-}
