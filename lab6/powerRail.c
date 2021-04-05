f89058092483f0aac89346c3cfbe9ade6a1ab665
Ryan Kinney <ryan.kinney@maine.edu>
2021-04-05 10:53:20 -0400

    Setup stdin and stdout for USART

--- lab6/powerRail.c
+++ lab6/powerRail.c
@@ -11,12 +11,16 @@
 
 void update_clock_speed(void);
 void USART0_init(void);
-void USART0_Transmit(uint8_t byte);
-uint8_t USART0_Receive(void);
+int USART0_Transmit(char byte, FILE* stream);
+int USART0_Receive(FILE* stream);
+
+// used for using stdin and stdout with USART
+FILE usart0_str = FDEV_SETUP_STREAM(USART0_Transmit, USART0_Receive, _FDEV_SETUP_RW);
 
 int main(){
 	update_clock_speed(); // adjust OSCCAL
 	USART0_init(); // initialize USART0
+	stdin = stdout = &usart0_str;
 
 
 	while(1); // busy loop
@@ -52,17 +56,25 @@ void USART0_init(void){
 
 	// Set the frame format: 8 data bits, 2 stop bits
 	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+
+	// setup stdin and stdout streams
+	stdin = stdout = &usart0_str;
 }
 
-void USART0_Transmit(uint8_t byte){
+int USART0_Transmit(char byte, FILE* stream){
+	// send newline characters as \r\n
+	if(byte == '\n')
+		USART0_Transmit('\r',stream);
+
 	// wait for the transmit buffer to empty
 	while(!(UCSR0A&(1<<UDRE0)));
 
 	// put the data into the buffer, sending it
 	UDR0 = byte;
+	return 0;
 }
 
-uint8_t USART0_Receive(void){
+int USART0_Receive(FILE* stream){
 	// wait for data to be recevied
 	while(!(UCSR0A&(1<<RXC0)));
 
